006f13c10423920176fdcd2e4c86af7094f70033
Victor Rolack <victorolack@gmail.com>
2022-11-05 12:18:45 -0300

    add home mobile view and burger menu added

--- src/components/layout/HeaderNav.js
+++ src/components/layout/HeaderNav.js
@@ -1,29 +1,140 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import logo from '../../assets/img/nav-logo/logo-buenoo.svg'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/img/nav-logo/logo-buenoo.svg";
+
+import { useScreenSize } from "../../hooks/useScreenSize";
 
 export const HeaderNav = () => {
+  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const { width, height } = useScreenSize();
+
+  console.log(width);
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+
+    setIsMenuClicked(!isMenuClicked);
+  };
+
+  const sizeHandle = () => {
+    if (width < 767) {
+      setIsMobile(true);
+    }
+  };
+
   return (
-    <header className='header'>
-      <div className="logo">
-        <img src={logo} alt="logo header" />
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/home" className={({isActive}) => (isActive ? "activated" : "")}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/portfolio" className={({isActive}) => (isActive ? "activated" : "")}>Portfolio</NavLink>
-          </li>
-          <li>
-            <NavLink to="/services" className={({isActive}) => (isActive ? "activated" : "")}>Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" className={({isActive}) => (isActive ? "activated" : "")}>Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className="header">
+      {width > 767 && (
+        <>
+          <div className="logo">
+            <img src={logo} alt="logo header" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/home"
+                  className={({ isActive }) =>
+                    isActive ? "activated" + burgerClass : ""
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/portfolio"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Portfolio
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/services"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </>
+      )}
+
+      {width < 767 && (
+        <>
+          <div className="logo">
+            <img src={logo} alt="logo header" />
+          </div>
+          <div className="burger-menu" onClick={updateMenu}>
+            <div className={burgerClass}></div>
+            <div className={burgerClass}></div>
+            <div className={burgerClass}></div>
+          </div>
+          <div className={menuClass}>
+            <ul>
+              <hr />
+              <li>
+                <NavLink
+                  to="/home"
+                  className={({ isActive }) =>
+                    isActive ? "activated" + burgerClass : ""
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <hr />
+              <li>
+                <NavLink
+                  to="/portfolio"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Portfolio
+                </NavLink>
+              </li>
+              <hr />
+              <li>
+                <NavLink
+                  to="/services"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Services
+                </NavLink>
+              </li>
+              <hr />
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? "activated" : "")}
+                >
+                  Contact
+                </NavLink>
+              </li>
+              <hr />
+            </ul>
+          </div>
+        </>
+      )}
     </header>
-  )
-}
+  );
+};
---
+++ src/hooks/useScreenSize.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+
+export const useScreenSize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeigth] = useState(window.innerHeight);
+
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    
+    return () => {
+      window.addEventListener("resize", handleResize)
+    }
+  }, [])
+
+
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+    setHeigth(window.innerHeight)
+  }
+
+
+  return {height, width}
+}
