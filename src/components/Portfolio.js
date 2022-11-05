7f4efc1178402c60cb9178e453b358d782044958
Victor Rolack <victorolack@gmail.com>
2022-11-05 18:00:40 -0300

    add content to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,34 +2,24 @@ import React from 'react'
 import { Banner } from './layout/Banner'
 import { Project } from './layout/Project'
 import banner from './../assets/img/banner/banner-home.jpg'
+import freeMovies from './../assets/img/projects/free_movies.png'
+import cesfamBackend from './../assets/img/projects/cesfam_backend.png'
 
 export const Portfolio = () => {
 
   const projects = [
     {
       name: 'Free Movies',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis turpis nec quam placerat, id convallis ligula lacinia. Phasellus eu ultricies risus. Donec ac fringilla dui. Nullam et sollicitudin orci. Suspendisse a convallis urna.',
-      img: banner,
+      description: "It's a simple project build in React for practice my state manegement. You can create a list of movies with his description, and all them will save on local storage.",
+      img: freeMovies,
       githubUrl: 'https://github.com/vcrolack/Free-movies'
     },
     {
-      name: 'Free Movies',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis turpis nec quam placerat, id convallis ligula lacinia. Phasellus eu ultricies risus. Donec ac fringilla dui. Nullam et sollicitudin orci. Suspendisse a convallis urna.',
-      img: banner,
-      githubUrl: 'https://github.com/vcrolack/Free-movies'
+      name: 'Cesfam backend',
+      description: "It's a project build in Fast API for a university project. Is for a simulation of a health center, so you can save information in a MySql database and consult these information.",
+      img: cesfamBackend,
+      githubUrl: 'https://github.com/vcrolack/farmacia-cesfam-backend'
     },
-    {
-      name: 'Free Movies',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis turpis nec quam placerat, id convallis ligula lacinia. Phasellus eu ultricies risus. Donec ac fringilla dui. Nullam et sollicitudin orci. Suspendisse a convallis urna.',
-      img: banner,
-      githubUrl: 'https://github.com/vcrolack/Free-movies'
-    },
-    {
-      name: 'Free Movies',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis turpis nec quam placerat, id convallis ligula lacinia. Phasellus eu ultricies risus. Donec ac fringilla dui. Nullam et sollicitudin orci. Suspendisse a convallis urna.',
-      img: banner,
-      githubUrl: 'https://github.com/vcrolack/Free-movies'
-    }
   ]
 
   return (
