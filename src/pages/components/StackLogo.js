d2453ee80b769f0331bb3325d89267d51f5b5d6f
Victor Rolack <victorolack@gmail.com>
2024-05-31 19:49:02 -0400

    fix stack logo

--- src/pages/components/StackLogo.js
+++ src/pages/components/StackLogo.js
@@ -3,9 +3,13 @@ import React from "react";
 export const StackLogo = ({ logo, page, width='70px', height='70px' }) => {
   return (
     <div className="stack-logo" style={{width, height}}>
-      <a target="_blank" href={page ? page : ''}>
+      { page ? (
+        <a target="_blank" href={page}>
+          <img src={logo} alt="It's a logo." />
+        </a>
+      ) : (
         <img src={logo} alt="It's a logo." />
-      </a>
+      )}
     </div>
   );
 };
